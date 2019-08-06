2bce634db7c6f02a022d9ed43e1986b8da6b8222
credd3 <creddy4@sapient.com>
2019-08-06 16:45:21 +0530

    Production routes

--- src/actions/databaseRoutes.js
+++ src/actions/databaseRoutes.js
@@ -1,10 +1,10 @@
-export const GET_TODOS_URL = 'http://localhost:5000/getTodos';
-export const ADD_TODO_URL = 'http://localhost:5000';
-export const DELETE_TODO_URL = 'http://localhost:5000';
-export const TOGGLE_TODO_URL = 'http://localhost:5000';
+export const GET_TODOS_URL = 'https://limitless-brushlands-35057.herokuapp.com/getTodos';
+export const ADD_TODO_URL = 'https://limitless-brushlands-35057.herokuapp.com';
+export const DELETE_TODO_URL = 'https://limitless-brushlands-35057.herokuapp.com';
+export const TOGGLE_TODO_URL = 'https://limitless-brushlands-35057.herokuapp.com';
 
 export const CHANGE_VISIBILITY_FILTER_URL = '';
 
-export const LOGIN_USER_URL = 'http://localhost:5000/login';
-export const LOGOUT_USER_URL = 'http://localhost:5000/logout';
-export const REGISTER_USER_URL = 'http://localhost:5000/register';
+export const LOGIN_USER_URL = 'https://limitless-brushlands-35057.herokuapp.com/login';
+export const LOGOUT_USER_URL = 'https://limitless-brushlands-35057.herokuapp.com/logout';
+export const REGISTER_USER_URL = 'https://limitless-brushlands-35057.herokuapp.com/register';
