c2a0157c4271482474b6f04ea42e281ca022da89
credd3 <creddy4@sapient.com>
2019-08-07 18:31:35 +0530

    Styling Changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,8 @@ import Form from '../Form/form';
 class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand mb-0 h1" style={{color: "greenyellow"}}>{this.props.username}</span>
+      <nav className="navbar bg-dark">
+        <span className="navbar-brand mb-0 h1" style={{color: "blanchedalmond"}}>{this.props.username}</span>
         <Form />
         <button className="btn btn-warning logoutButton" onClick={this.props.logout}>Log Out</button>
       </nav>
