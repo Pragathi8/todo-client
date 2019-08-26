9df944a20fdc16a45ac34d7bd007da647664354b
credd3 <creddy4@sapient.com>
2019-08-26 16:41:59 +0530

    Offline Display

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('/serviceWorker.js')
-    .then((reg) => console.log(`Service Worker Registered ${reg}`))
     .catch((err) => console.log(`unable to register serviceWorker--> ${err}`))
 } 
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -4,6 +4,7 @@ const STATIC_CACHE = 'STATIC_CACHE';
 const DYNAMIC_CACHE = 'DYNAMIC_CACHE';
 
 const staticAssets = [
+    '/',
     '/app.js',
     '/manifest.json',
     '/icons/icon-64.png',
@@ -12,8 +13,10 @@ const staticAssets = [
     '/icons/icon-512.png',
 ]
 
-self.addEventListener('install', () => {
-    caches.open(STATIC_CACHE).then(cache => cache.addAll(staticAssets))
+self.addEventListener('install', evt => {
+    console.log(`install event`);
+    evt.waitUntil(caches.open(STATIC_CACHE).then(cache => cache.addAll(staticAssets)));
+    
 })
 
 self.addEventListener('activate', () => {
@@ -22,6 +25,12 @@ self.addEventListener('activate', () => {
 
 self.addEventListener('fetch', evt => {
     evt.respondWith(
-        caches.match(evt.request).then(cacheRes => cacheRes || fetch(evt.request))
+        caches.match(evt.request).then(cacheRes => cacheRes || fetch(evt.request).then(fetchRes => {
+            if(evt.request.url.includes('chrome-extension')) return fetchRes;
+            return caches.open(DYNAMIC_CACHE).then(cache => {
+                cache.put(evt.request.url, fetchRes.clone())
+                return fetchRes;
+            })
+        }))
     )
 })
